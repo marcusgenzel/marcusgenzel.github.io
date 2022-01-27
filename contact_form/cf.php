8503fc271e6051957c948c5d5fd7a6ebe8eda838
marcusgenzel <marcusgenzel@gmx.net>
2022-01-27 21:43:19 +0100

    Update cf.php

--- contact_form/cf.php
+++ contact_form/cf.php
@@ -4,15 +4,15 @@
 $name = $_POST['name'];
 $email= $_POST['email'];
 $message= $_POST['message'];
-$telefon= $_POST['telefon'];
+$number= $_POST['telefon'];
 
 $to = "marcusgenzel@gmx.net";
 $subject = "Mail From website";
-$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message."\r\n Telefonnummer = ". $telefon;
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message."\r\n Telefonnummer = ". $number;
 $headers = "From: noreply@yoursite.com" . "\r\n" .
 "CC: ";
 if($email!=NULL){
-    mail($to,$subject,$txt,$headers);
+mail($to,$subject,$txt,$headers);
 }
 //redirect
 header("Location:thankyou.html");
